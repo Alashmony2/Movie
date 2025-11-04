f0cb71189e3ba0304e56ee4dfaab72398123699c
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-11-04 20:05:40 +0200

    install tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-3xl font-bold underline'>Eng Alashmony</h1>
+      
+    </>
+  )
+}
+
+export default App
