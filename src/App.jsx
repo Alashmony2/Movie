6612d1db7ff7908ee47b568c8d083b03c76a9bb3
Alashmony2 <ahmedSamyAlashmony1@gmail.com>
2025-11-30 20:42:52 +0200

    prepare image

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,16 @@ function App() {
 
   return (
     <>
-      
-      <h1 className='text-3xl font-bold underline'>Eng Alashmony</h1>
-      
+      <main>
+        <div className="pattern"/>
+        <div className="wrapper">
+          <img src="../public/hero-img.png" alt="hero panner" />
+          <header>
+            <h1 className='capitalize'>Find <span className='text-gradient'>movies</span>  you'll enjoy without the hassle</h1>
+          </header>
+        </div>
+      </main>
+
     </>
   )
 }
