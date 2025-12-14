f891008a1ee9371a3983462d3584ccc5d9ce5582
Alashmony2 <ahmedSamyAlashmony1@gmail.com>
2025-12-15 00:49:59 +0200

    add api's base url

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
 import Search from './components/Search.jsx'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
 function App() {
 
   const [searchTerm, setSearchTerm] = useState('')
+  useEffect(() => { }, [])
 
   return (
     <>
       <main>
         <div className="pattern" />
         <div className="wrapper">
-          <img src="hero.png" alt="hero panner" />
           <header>
+            <img src="hero.png" alt="hero panner" />
             <h1 className='capitalize'>Find <span className='text-gradient'>movies</span>  you'll enjoy without the hassle</h1>
           </header>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
