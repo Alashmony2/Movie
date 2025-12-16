aa70cc663547742e52dd33743afa6062f780f303
Alashmony2 <ahmedSamyAlashmony1@gmail.com>
2025-12-16 11:48:26 +0200

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,45 @@ import { useEffect, useState } from 'react'
 import Search from './components/Search.jsx'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 function App() {
 
-  const [searchTerm, setSearchTerm] = useState('')
-  useEffect(() => { }, [])
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data")
+      }
+      const data = await response.json();
+      console.log(data);
+      if (data.response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+
+    } catch (error) {
+      console.log(error);
+      setErrorMessage("Error fetching movies . please try again later .");
+    }
+  }
+  useEffect(() => {
+    fetchMovies();
+  }, [])
 
   return (
     <>
@@ -15,9 +50,13 @@ function App() {
           <header>
             <img src="hero.png" alt="hero panner" />
             <h1 className='capitalize'>Find <span className='text-gradient'>movies</span>  you'll enjoy without the hassle</h1>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-          <h1 className="text-white text-2xl">{searchTerm}</h1>
+          <section className='all-movies'>
+            <h2>All Movies</h2>
+
+            {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+          </section>
         </div>
       </main>
 
